43578c60e0abbf8eb5e1ba45f865761125c53ae6
Ryan Quey <quey.ryan@gmail.com>
2021-09-01 17:05:23 +0700

    use up to date spring-cloud-schema-registry-client instead of spring-cloud-stream-schema

--- stream-processors/location-processor/src/main/java/us/anant/dataflow/sample/locationprocessor/LocationProcessorApplication.java
+++ stream-processors/location-processor/src/main/java/us/anant/dataflow/sample/locationprocessor/LocationProcessorApplication.java
@@ -3,10 +3,10 @@ package us.anant.dataflow.sample.locationprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
+import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient;
 
 import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.cloud.stream.schema.avro.AvroSchemaMessageConverter;
+import org.springframework.cloud.schema.registry.avro.AvroSchemaMessageConverter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.MimeType;
